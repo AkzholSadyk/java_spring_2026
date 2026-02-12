83213e1cb85665bf7e01f1a330759880f0e73d55
Akzhol Sadyk <akzholsadyk12@gmail.com>
2026-02-12 16:49:35 +0500

    ?

--- lab1/src/main/java/com/example/Spring2026/controller/InfoController.java
+++ lab1/src/main/java/com/example/Spring2026/controller/InfoController.java
@@ -11,7 +11,7 @@ public class InfoController {
     @GetMapping("/api/info")
     public Map<String, Object> getInfo() {
         return Map.of(
-                "student", "Nurik Demesinov",
+                "student", "Ako SADYK",
                 "course", "Spring Framework",
                 "week", 1
         );
